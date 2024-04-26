098359552502d368ea9a44bc7b3f6e0e7f5cf218
Andrejs Dāvis Umbrovskis <ledaviskylv@gmail.com>
2024-04-26 15:48:48 +0200

    Constant values

---
+++ TinyTankSurvival/Assets/Scripts/Constants.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class Constants
+{
+    /// <summary>
+    /// Player's character name in the scene
+    /// </summary>
+    public const string PlayerCharacterName = "PlayerCharacter";
+    /// <summary>
+    /// Player or Enemy character Tank child name
+    /// </summary>
+    public const string TankName = "Tank";
+}
